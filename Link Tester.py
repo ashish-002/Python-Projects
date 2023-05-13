bb1eb687baf65cbe039ca359a91624342e4b5b73
ashish-002 <44707624+ashish-002@users.noreply.github.com>
2023-05-13 17:49:01 +0530

    Add files via upload

---
+++ Link Tester.py
@@ -0,0 +1,34 @@
+import tkinter as tk
+from tkinter import messagebox
+import requests
+
+def test_link():
+    link = entry.get()
+
+    try:
+        response = requests.get(link)
+
+        if response.status_code == 200:
+            messagebox.showinfo("Link Test", "Link is working fine!")
+        else:
+            messagebox.showerror("Link Test", "Link is not working. Status code: {}".format(response.status_code))
+    except requests.exceptions.RequestException as e:
+        messagebox.showerror("Link Test", "An error occurred: {}".format(str(e)))
+
+# Create the main window
+window = tk.Tk()
+window.title("Link Tester")
+
+# Create the label and entry widget for entering the link
+label = tk.Label(window, text="Enter a link:")
+label.pack()
+
+entry = tk.Entry(window, width=50)
+entry.pack()
+
+# Create the button for testing the link
+button = tk.Button(window, text="Test Link", command=test_link)
+button.pack()
+
+# Run the main event loop
+window.mainloop()
